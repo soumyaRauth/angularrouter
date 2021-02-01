10cbcf893d792c2aca5df9d0ce8cbeb31ad3a99d
soumyaRauth <shoummorauth@gmail.com>
2021-02-02 00:58:01 +0600

    angular router tutorial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'animals',
+    component:AnimalsComponent,
+    children:[
+      {
+        path:':name',
+        component:AnimalDetailComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AnimalsComponent,
+    AnimalDetailComponent
   ],
   imports: [
     BrowserModule,
